ec9212aca01d19592d3a8962d7f58efb193223ed
Camus10 <mharishidayat@gmail.com>
2024-03-23 09:10:52 +0700

    fix dashboard

---
+++ js/dashboard/dashboard.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  // Retrieve access token from sessionStorage
+  var accessToken = sessionStorage.getItem('accessToken');
+  
+  // Make an AJAX request to fetch user data
+  $.ajax({
+    url: `${API_BASE_URL}/auth`,
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken // Include access token in request headers
+    },
+    success: function(response) {
+      // Populate dropdown with user data
+      $('#userName').text(response.data.username);
+      // $('#userRole').text(response.data.role);
+      // $('#userAvatar').attr('src', response.data.avatar);
+      // Greeting message
+      $('#greetingHeader').text('Hello, ' + response.data.username + '!');
+    },
+    error: function(xhr, status, error) {
+      console.error('Error fetching user data:', error);
+    }
+  });
+});
+
+
+// Add click event to logout button
+$("#logoutButton").click(function(event){
+  event.preventDefault(); // Prevent the default action of the link
+
+  // Remove user data and user ID from sessionStorage
+  sessionStorage.removeItem('accessToken');
+  // sessionStorage.removeItem('userId');
+
+  // Redirect to the login page
+  window.location.href = '../../login.html';
+});
